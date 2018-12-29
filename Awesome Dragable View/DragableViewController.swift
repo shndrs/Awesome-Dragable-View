dafd42e6311112b03c53b0da60b2749da246909c
Sahand Raeisi <sahandraeisi@yahoo.com>
2018-12-29 10:29:33 +0330

    create project

---
+++ Awesome Dragable View/DragableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DragableViewController.swift
+//  Awesome Dragable View
+//
+//  Created by Sahand Raeisi on 12/29/18.
+//  Copyright © 2018 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+
+class DragableViewController: UIViewController {
+
+    @IBOutlet weak var handleArea: UIView!
+
+
+    
+}
---
+++ Awesome Dragable View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Awesome Dragable View
+//
+//  Created by Sahand Raeisi on 12/29/18.
+//  Copyright © 2018 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
